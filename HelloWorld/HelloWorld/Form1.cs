558de132110e3b51c9d110b6f3f278d31494ef46
Peter <superpeter2005@yandex.ru>
2018-07-17 21:58:01 +0300

    mas com 2

--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -25,6 +25,7 @@ namespace HelloWorld
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello test!");
+            MessageBox.Show("!");
         }
     }
 }
